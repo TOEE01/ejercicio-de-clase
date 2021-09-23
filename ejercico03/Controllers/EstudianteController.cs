d3a4a88df516074a58f2e8bd82226c5d0c1764ef
TOEE01 <recinos.melvin32@gmail.com>
2021-09-23 14:24:04 -0600

    cambios

---
+++ ejercico03/Controllers/EstudianteController.cs
@@ -0,0 +1,35 @@
+﻿using ejercico03.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ejercico03.Controllers
+{
+    public class EstudianteController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index(Estudiante estudiante)
+        {
+            if (ModelState.IsValid)
+            {
+                return View ("Registro");
+            }
+            else
+            {
+                return Redirect("Estudiante/Index");
+            }
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Registro()
+        {
+            return View();
+        }
+    }
+}
---
+++ ejercico03/Models/Estudiante.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ejercico03.Models
+{
+    public class Estudiante
+    {
+        [Display(Name = "Nombre")]
+        [Required(ErrorMessage ="Este campo {0} es requerido")]
+        public string Nombre { get; set; }
+
+
+        //[Display(Name = "Edad")]
+        [Required(ErrorMessage = "Este campo {0} es requrido")]
+        public int Edad { get; set; }
+
+
+        //[Display(Name = "TipoDeEstudiante")]
+        [Required(ErrorMessage = "Este campo {0} es requrido")]
+        public string TipoDeEstudiante { get; set; }
+
+
+        //[Display(Name = "Sexo")]
+        [Required(ErrorMessage = "Este campo {0} es requrido")]
+        public char Sexo { get; set; }
+        
+
+    }
+}
